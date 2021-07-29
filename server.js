3c9a4f3939e548f017c562f86cffb8c2a14e1f3c
Douglas <dz@bitzend.net>
2021-07-28 20:34:45 -0600

    no commas there please

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.post('/create-checkout-session', async (req, res) => {
   const pmTypes = [
       'card',
       'wallet',
-    ],
+    ];
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: pmTypes,
