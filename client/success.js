8683b633252ade84b290718cdfe2a577db254697
Douglas <dz@bitzend.net>
2021-07-29 09:55:54 -0600

    add aws to success

--- client/success.js
+++ client/success.js
@@ -1,6 +1,40 @@
 var urlParams = new URLSearchParams(window.location.search);
 var sessionId = urlParams.get('session_id');
 
+const AWS = require('aws-sdk')
+
+
+AWS.config.update({
+
+  accessKeyId: '???',
+
+  secretAccessKey: '???'
+
+})
+
+
+const s3 = new AWS.S3()
+
+const myBucket = 'naturistic'
+
+const myKey = 'mountains/desert_mountain.jpg'
+
+const signedUrlExpireSeconds = 60 * 5 // 5 minutes
+
+
+const url = s3.getSignedUrl('getObject', {
+
+    Bucket: myBucket,
+
+    Key: myKey,
+
+    Expires: signedUrlExpireSeconds
+
+})
+
+
+console.log(url)
+
 if (sessionId) {
   fetch('/checkout-session?sessionId=' + sessionId)
     .then(function (result) {
@@ -13,4 +47,4 @@ if (sessionId) {
     .catch(function (err) {
       console.log('Error when fetching Checkout session', err);
     });
-} 
+}
