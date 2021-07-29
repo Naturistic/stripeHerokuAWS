970ec0111a54448b9a0dae633ba616cf68d01551
Douglas <dz@bitzend.net>
2021-07-29 12:05:59 -0600

    rmove  server side cor

--- client/success.js
+++ client/success.js
@@ -2,36 +2,6 @@ var urlParams = new URLSearchParams(window.location.search);
 var sessionId = urlParams.get('session_id');
 
 
-AWS.config.update({
-
-  accessKeyId: '???',
-
-  secretAccessKey: '???'
-
-})
-
-
-const s3 = new AWS.S3()
-
-const myBucket = 'naturistic'
-
-const myKey = 'mountains/desert_mountain.jpg'
-
-const signedUrlExpireSeconds = 60 * 5 // 5 minutes
-
-
-const url = s3.getSignedUrl('getObject', {
-
-    Bucket: myBucket,
-
-    Key: myKey,
-
-    Expires: signedUrlExpireSeconds
-
-})
-
-
-console.log(url)
 
 if (sessionId) {
   fetch('/checkout-session?sessionId=' + sessionId)
